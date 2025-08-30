91756e1cac76c96b6277c654c1e177e00bd08cd5
Emjey <reach2jeyan@outlook.com>
2025-08-30 21:29:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 set -e
 
+echo "🚀 Entrypoint running..."
+
 args="${INPUT_PYTEST_ARGS}"
 
+if [ "${INPUT_JSON_REPORT}" != "" ]; then
+  args="$args --json-report=${INPUT_JSON_REPORT}"
+fi
+if [ "${INPUT_HTML_OUTPUT}" != "" ]; then
+  args="$args --html-output=${INPUT_HTML_OUTPUT}"
+fi
+if [ "${INPUT_SCREENSHOTS}" != "" ]; then
+  args="$args --screenshots=${INPUT_SCREENSHOTS}"
+fi
+if [ "${INPUT_PLUS_EMAIL}" = "true" ]; then
+  args="$args --plus-email"
+fi
+if [ "${INPUT_GENERATE_XML}" = "true" ]; then
+  args="$args --generate-xml"
+fi
+if [ "${INPUT_XML_REPORT}" != "" ]; then
+  args="$args --xml-report=${INPUT_XML_REPORT}"
+fi
+if [ "${INPUT_CAPTURE_SCREENSHOTS}" != "" ]; then
+  args="$args --capture-screenshots=${INPUT_CAPTURE_SCREENSHOTS}"
+fi
+if [ "${INPUT_SHOULD_OPEN_REPORT}" != "" ]; then
+  args="$args --should-open-report=${INPUT_SHOULD_OPEN_REPORT}"
+fi
 
-[ -n "${INPUT_JSON_REPORT}" ] && args="$args --json-report=${INPUT_JSON_REPORT}"
-[ -n "${INPUT_CAPTURE_SCREENSHOTS}" ] && args="$args --capture-screenshots=${INPUT_CAPTURE_SCREENSHOTS}"
-[ -n "${INPUT_HTML_OUTPUT}" ] && args="$args --html-output=${INPUT_HTML_OUTPUT}"
-[ -n "${INPUT_SCREENSHOTS}" ] && args="$args --screenshots=${INPUT_SCREENSHOTS}"
-[ "${INPUT_PLUS_EMAIL}" = "true" ] && args="$args --plus-email"
-[ "${INPUT_GENERATE_XML}" = "true" ] && args="$args --generate-xml"
-[ -n "${INPUT_XML_REPORT}" ] && args="$args --xml-report=${INPUT_XML_REPORT}"
-[ -n "${INPUT_DETECT_FLAKE}" ] && args="$args --detect-flake=${INPUT_DETECT_FLAKE}"
-[ -n "${INPUT_SHOULD_OPEN_REPORT}" ] && args="$args --should-open-report=${INPUT_SHOULD_OPEN_REPORT}"
-
-echo "Running pytest with args: $args"
-
+echo "👉 Final pytest command: pytest $args"
 pytest $args
